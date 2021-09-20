55260c3e4eef47d88564b9a0a121a3b23130ed3a
Fadhil Amadan <git@fadhilamadan.com>
2021-09-20 22:19:25 +0700

    feat(branch): add branch update

---
+++ src/components/branch/editModal.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+
+import { getBranchUpdate } from '../../redux/middleware/branch/branch.middleware';
+import { timeStatic } from '../../utils/const/time';
+
+import { connect } from 'react-redux';
+
+const BranchEditModal = (props) => {
+  const [branchEdit, setBranchEdit] = useState({
+    name: props.item.name,
+    latitude: props.item.latitude,
+    longitude: props.item.longitude,
+    opening_hours: props.item.opening_hours,
+  });
+
+  const handleUpdate = (branchId) => {
+    props.onGetBranchUpdate(branchId, branchEdit);
+    props.setModal(false);
+  };
+
+  return (
+    <div className="modal modal-open">
+      <div className="modal-box">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Branch Name</span>
+          </label>
+          <input
+            className="input input-bordered"
+            onChange={(e) =>
+              setBranchEdit((val) => ({ ...val, name: e.target.value }))
+            }
+            placeholder="Branch Name"
+            type="text"
+            value={branchEdit.name}
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Latitude</span>
+          </label>
+          <input
+            className="input input-bordered"
+            onChange={(e) =>
+              setBranchEdit((val) => ({ ...val, latitude: e.target.value }))
+            }
+            placeholder="Latitude"
+            type="text"
+            value={branchEdit.latitude}
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Longitude</span>
+          </label>
+          <input
+            className="input input-bordered"
+            onChange={(e) =>
+              setBranchEdit((val) => ({ ...val, longitude: e.target.value }))
+            }
+            placeholder="Longitude"
+            type="text"
+            value={branchEdit.longitude}
+          />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Opening Hour</span>
+          </label>
+          <select
+            className="select select-bordered"
+            onChange={(e) =>
+              setBranchEdit((val) => ({ ...val, opening_hours: e.target.value }))
+            }
+          >
+            <option selected="selected" value="">
+              Select
+            </option>
+
+            {timeStatic.map((item) => {
+              return (
+                <>
+                  <option
+                    selected={branchEdit.opening_hours === item && 'selected'}
+                    value={item}
+                  >
+                    {item}
+                  </option>
+                </>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="modal-action">
+          <a className="btn" onClick={() => props.setModal(false)}>
+            Cancel
+          </a>
+          <a className="btn btn-primary" onClick={() => handleUpdate(props.item.id)}>
+            Update
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetBranchUpdate: (branchId, data) => {
+      return dispatch(getBranchUpdate(branchId, data));
+    },
+  };
+};
+
+export default connect(mapState, mapDispatchToProps)(BranchEditModal);
--- src/components/branch/list/card.js
+++ src/components/branch/list/card.js
@@ -1,62 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import BranchEditModal from '../../branch/editModal';
 import BuffetCard from '../../buffet/card';
 
 import _ from 'lodash';
 import rupiah from 'rupiah-format';
 
 const BranchCard = (props) => {
+  const [modal, setModal] = useState(false);
+  const [modalType, setModalType] = useState();
+
+  const handleAction = (branchId, type) => {
+    setModal(true);
+    setModalType(type);
+  };
+
   return (
-    <div className="card compact shadow">
-      <div className="card-body">
-        <h2 className="card-title pb-2 text-2xl">{props.item.name}</h2>
+    <>
+      <div className="card compact shadow">
+        <div className="card-body">
+          <h2 className="card-title pb-2 text-2xl">{props.item.name}</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-          {props.item.Buffets.length === 0 ? (
-            <div key="key" className="card compact shadow">
-              <div className="card-body">
-                <h4 className="card-title">Don&apos;t have any Branch yet.</h4>
-              </div>
-            </div>
-          ) : (
-            _(props.item.Buffets)
-              .groupBy((item) => item.day)
-              .map((day, iDay) => (
-                <div key={iDay} className="card shadow">
-                  <div className="card-body">
-                    <h4 className="card-title text-md pb-2">
-                      {iDay} - Price from
-                      <div className="badge ml-2">
-                        {rupiah.convert(day[0].price)}
-                      </div>
-                    </h4>
-
-                    {day.map((buffet, iBuffet) => (
-                      <BuffetCard key={iBuffet} item={buffet} />
-                    ))}
-                  </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+            {props.item.Buffets.length === 0 ? (
+              <div key="key" className="card compact shadow">
+                <div className="card-body">
+                  <h4 className="card-title">Don&apos;t have any Branch yet.</h4>
                 </div>
-              ))
-              .value()
-          )}
-        </div>
+              </div>
+            ) : (
+              _(props.item.Buffets)
+                .groupBy((item) => item.day)
+                .map((day, iDay) => (
+                  <div key={iDay} className="card shadow">
+                    <div className="card-body">
+                      <h4 className="card-title text-md pb-2">
+                        {iDay} - Price from
+                        <div className="badge ml-2">
+                          {rupiah.convert(day[0].price)}
+                        </div>
+                      </h4>
 
-        <div className="card-actions">
-          <button
-            className="btn btn-warning btn-sm"
-            onClick={() => console.log('edit branch')}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-sm"
-            onClick={() => console.log('delete branch')}
-          >
-            Delete
-          </button>
+                      {day.map((buffet, iBuffet) => (
+                        <BuffetCard key={iBuffet} item={buffet} />
+                      ))}
+                    </div>
+                  </div>
+                ))
+                .value()
+            )}
+          </div>
+
+          <div className="card-actions">
+            <button
+              className="btn btn-warning btn-sm"
+              onClick={() => handleAction(props.item, 'EDIT')}
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-sm"
+              onClick={() => handleAction(props.item, 'DELETE')}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {modal && modalType === 'EDIT' && (
+        <BranchEditModal item={props.item} setModal={setModal} />
+      )}
+    </>
   );
 };
 
