e52741e94f876bc1e493b7c20584761c3c49085b
Fadhil Amadan <git@fadhilamadan.com>
2021-09-21 07:05:56 +0700

    refactor(branch): improve filter opening hour

--- src/components/branch/list/index.js
+++ src/components/branch/list/index.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect, useState } from 'react';
 
 import { getBranch } from '../../../redux/middleware/branch/branch.middleware';
+import { timeStatic } from '../../../utils/const/time';
 
 import BranchCard from './card';
 import BranchCardLoader from './cardLoader';
@@ -60,21 +61,14 @@ const Branch = (props) => {
           <option selected="selected" value="">
             Opening hour
           </option>
-          <option value="07:00">07:00</option>
-          <option value="08:00">08:00</option>
-          <option value="09:00">09:00</option>
-          <option value="10:00">10:00</option>
-          <option value="11:00">11:00</option>
-          <option value="12:00">12:00</option>
-          <option value="13:00">13:00</option>
-          <option value="14:00">14:00</option>
-          <option value="15:00">15:00</option>
-          <option value="16:00">16:00</option>
-          <option value="17:00">17:00</option>
-          <option value="18:00">18:00</option>
-          <option value="19:00">19:00</option>
-          <option value="20:00">20:00</option>
-          <option value="21:00">21:00</option>
+
+          {timeStatic.map((item) => {
+            return (
+              <>
+                <option value={item}>{item}</option>
+              </>
+            );
+          })}
         </select>
 
         <button className="btn btn-primary" onClick={() => handleFilter()}>
